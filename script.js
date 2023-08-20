1dd960db8db2140b59fdb8fa602cd7c27ff7896c
sakib071 <mdsakibchy071@gmail.com>
2023-08-20 21:30:44 +0600

    js started 2.0

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-function getInputValueById(inputId) {
-    const inputElement = document.getElementById(inputId);
-    const inputElementValue = inputElement.innerText;
-    return inputElementValue;
+const cartHistory = document.querySelector('#item-list');
+const totalPriceElement = document.querySelector('#total-price');
+
+let totalPrice = 0;
+
+function updateTotalPrice() {
+    totalPriceElement.innerText = totalPrice.toFixed(2);
 }
 
-document.getElementById("card-one").addEventListener("click", function () {
-    const itemCard = getInputValueById("item-one");
-    // const item = itemCard.innerText;
-    console.log(itemCard);
-})
+function addToCartHistory(itemName, itemPrice) {
+    const autoNumber = cartHistory.querySelectorAll('p.text-md.font-semibold').length + 1;
+    const itemInfo = document.createElement('p');
+    itemInfo.classList.add('text-md', 'font-semibold');
+    itemInfo.innerHTML = `<span>${autoNumber}. ${itemName}</span>`;
+    cartHistory.appendChild(itemInfo);
+
+    totalPrice += itemPrice;
+    updateTotalPrice();
+}
+
+const productCards = document.querySelectorAll('.card-one');
+productCards.forEach(card => {
+    const itemName = card.querySelector('.card-title').innerText;
+    const itemPrice = parseFloat(card.querySelector('.card-body p').innerText);
+
+    card.addEventListener('click', () => {
+        addToCartHistory(itemName, itemPrice);
+    });
+});
